e37c15a73a6299c4b0d4edec1a37769dc88b2a8f
maciej <maciej@debian>
2024-04-18 11:29:29 +0200

    NumPy interfacing

--- spam/spammodule.c
+++ spam/spammodule.c
@@ -1,16 +1,18 @@
 #define PY_SSIZE_T_CLEAN
-#include <python3.10/Python.h>
-
-
+#include <python3.11/Python.h>
+#include <python3.11/numpy/arrayobject.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 static PyObject * spam_system(PyObject * self,PyObject *args){
-    const char * command;
-    int sts;
-    if(!PyArg_ParseTuple(args,"s",&command)){
-        return NULL;
-    }
-    sts = system(command);
-    return PyLong_FromLong(sts);
+	//PyArrayObject* arr = (PyArrayObject*) PyArray_FromAny(args, NULL, 0,0, 
+	//		NPY_ARRAY_IN_ARRAY,NULL);
+	//
+    PyArrayObject *arr = (PyArrayObject *)PyArray_FromAny(args, NULL, 0, 0, NPY_ARRAY_IN_ARRAY, NULL);	
+	for(int i = 0; i < arr->nd; i++){
+		printf("%d\n", arr->dimensions[i]);	
+	}
+	return PyLong_FromLong(0);
 }
 
 
@@ -27,6 +29,7 @@ static struct PyModuleDef spammodule = {
     SpamMethods
 };
 PyMODINIT_FUNC PyInit_spam(void){
+	import_array();
     return PyModule_Create(&spammodule);
 }
 

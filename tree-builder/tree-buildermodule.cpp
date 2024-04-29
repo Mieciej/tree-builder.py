87a58918b441572c40584671066737ff2b46404f
Maciej <>
2024-04-29 16:47:58 +0200

    Code cleanup - created new functions on repeating parts of the code

--- tree-builder/tree-buildermodule.cpp
+++ tree-builder/tree-buildermodule.cpp
@@ -4,6 +4,7 @@
 #include "attribute.h"
 #include <vector>
 #include "tree.h"
+#include <unordered_map>
 
 extern "C" void expand_tree(Branch* b);
 
@@ -11,14 +12,35 @@ extern "C" void printBT(const Branch* node, long value,
                         std::unordered_map<long, std::string> &dictionary);
 extern "C" void print_tree_helper(const std::string& prefix, const Branch* node,
                                   bool not_last, long value,std::unordered_map<long, std::string> &dictionary);
-extern "C" int build_branch(PyObject *list, bitmask_t* rows,
-                 Py_ssize_t n_rows, Py_ssize_t n_columns ) {
-    std::vector<Attribute*> * attributes
-        = new std::vector<Attribute*>();
+int parse_value(PyObject * value, std::string& result){
+
+    PyObject* str = PyObject_Str(value);
+    const char* bytes = PyUnicode_AsUTF8(str);
+    result =std::string(bytes);
+    return 0;
+}
+int encode_value(std::string key_value, 
+                 std::unordered_map<std::string, long> & encoding,
+                 std::unordered_map<long, std::string>* & decoding,
+                 long&  encoder){
+    if(!encoding.count(key_value)) {
+        (encoding)[key_value] = encoder++;
+        (*decoding)[encoder-1] = key_value;
+        std::cout <<"Value: " <<key_value <<
+        ", encoding: "<<(encoding)[key_value] <<std::endl;
+    }
+    return 0;
+}
+int parse_attributes(PyObject *list, Py_ssize_t n_rows,
+                     Py_ssize_t n_columns, std::vector<Attribute*>* &attributes,
+                     std::unordered_map<long, std::string> * &dictionary,
+                     long * &classes){
+
+     attributes = new std::vector<Attribute*>();
     PyObject *item, *value, *target;
     std::unordered_map<std::string,long> encoding; 
-    std::unordered_map<long, std::string> dictionary;
-    long *classes = new long [n_rows];
+    dictionary = new std::unordered_map<long, std::string>;
+    classes = new long [n_rows];
     long encoder = 0;
     for (Py_ssize_t row = 0; row < n_rows; row++) {
 
@@ -30,46 +52,51 @@ extern "C" int build_branch(PyObject *list, bitmask_t* rows,
                 return (int)NULL;
             }
             value = PyList_GetItem(item,column);
-            PyObject* str = PyObject_Str(value);
-            const char* bytes = PyUnicode_AsUTF8(str);
-            std::string key_value (bytes);
-            if(!encoding.count(key_value)) {
-                encoding[key_value] = encoder++;
-                dictionary[encoder-1] = key_value;
-                std::cout <<"Value: " <<key_value <<
-                ", encoding: "<<encoding[key_value] <<std::endl;
-            }
+            std::string key_value;
+            parse_value(value, key_value);
+        
+            encode_value(key_value, encoding, 
+                         dictionary, encoder);
 
             target = PyList_GetItem(item,n_columns-1);
-            str = PyObject_Str(target);
-            bytes = PyUnicode_AsUTF8(str);
-            std::string key_target (bytes);
-            if(!encoding.count(key_target)) {
-                encoding[key_target] = encoder++;
-                dictionary[encoder-1] = key_target;
-                std::cout <<"Target class: " <<key_target <<
-                ", encoding: "<<encoding[key_target] <<std::endl;
-            }
-
+            std::string key_target;
+            parse_value(target, key_target);
+            encode_value(key_target, encoding, 
+                         dictionary, encoder);
             classes[row] = encoding[key_target];
             std::pair<long,long> * pair = new std::pair<long,long>(encoding[key_value],encoding[key_target]) ;
-            if((unsigned)column < attributes->size()){
-                attributes->at(column)->values->push_back(pair);
-                continue;
+            Attribute* new_attribute;
+            if(row == 0){
+                new_attribute = new Attribute(n_rows,column);
+                attributes->push_back(new_attribute);
             }
-            Attribute* new_attribute = new Attribute(n_rows,column);
+            else {
+                new_attribute = (*attributes)[column];
+            }
 
             new_attribute->values->push_back(pair);
-            attributes->push_back(new_attribute);
         }
     }
-    bitmask_t n[1];
-    n[0] = ~0;
     std::cout << "INFO: Finished reading features." <<std::endl;
+    return 0;
+}
+
+extern "C" int build_branch(PyObject *list, 
+                 Py_ssize_t n_rows, Py_ssize_t n_columns ) {
+    std::vector<Attribute*> * attributes;
+    long * classes;
+    std::unordered_map<long,std::string> *dictionary;
+    parse_attributes(list, n_rows, n_columns,
+                     attributes, dictionary,
+                     classes);
+    bitmask_t n[n_rows/sizeof(bitmask_t)+1];
+    for (size_t i = 0; i<n_rows/sizeof(bitmask_t)+1; i++){
+        n[i] = ~0;
+    }
     Branch root(n_rows, *attributes, n, classes);
     expand_tree(&root);
     std::cout << "INFO: Finished constructing a tree. "<<std::endl;
-    printBT(&root, 0, dictionary);
+    printBT(&root, 0, *dictionary);
     return 0;
 }
 
@@ -123,8 +150,6 @@ static PyObject * tree_build(PyObject * self,PyObject *args){
     if(n < 0){
         return NULL;
     }
-    bitmask_t *rows = new bitmask_t[n/64+1];
-    rows[0] = -1;
 
 
     item = PyList_GetItem(list, 0);
@@ -132,7 +157,7 @@ static PyObject * tree_build(PyObject * self,PyObject *args){
     if( l < 0) {
         return NULL;
     }
-    build_branch(list,rows,n,l);
+    build_branch(list,n,l);
     return PyLong_FromLong(0);
 }
 
